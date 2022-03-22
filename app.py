59e732094c7f050723353a423a6c825b57752420
Jayachandran9283 <75618599+Jayachandran9283@users.noreply.github.com>
2022-03-22 15:22:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import flask 
+from flask import Flask,render_template,request
+import joblib
+import pandas as pd
+import numpy as np
+app=Flask(__name__)
+model=joblib.load("pipe.pkl")
+@app.route("/")
+def app_run():
+    return render_template("index.html")
+@app.route("/predict",methods=["POST"])
+def predict():
+    test_pd=pd.DataFrame([request.form])
+    value=model.predict(test_pd)[0]
+    value=str(round(value,2))
+    return render_template("predict.html",price=value+" Lakhs ")
+    # return request.form
+if __name__=="__main__":
+    app.run(debug=True)
